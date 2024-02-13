9c7896e897750893fc5b9ac60d6e47b8582f155d
Jeammm <87964996+Jeammm@users.noreply.github.com>
2024-02-13 13:53:40 +0700

    clean comments

--- player/player.ino
+++ player/player.ino
@@ -24,47 +24,6 @@ bool isReady = false;
 bool betPlaced = false;
 bool pickStand = false;
 
-// typedef struct ready_message {
-//   char message[32]; //message
-//   int id;  //player number 
-//   bool is_ready;  // ready or not
-// } ready_message;
-
-// typedef struct bet_message {
-//   char message[32]; //message
-//   int id;  //player number 
-//   int amount;  // ready or not
-// } bet_message;
-
-// typedef struct hit_card_message {
-//   char message[32]; //message
-//   int id;  //player number 
-//   bool hit;  //player hit request 1 = hit, 0 = stand
-// } hit_card_message;
-
-// typedef struct draw_card_message {
-//   char message[32]; //message
-//   int card[5];  //player number 
-// } draw_card_message;
-
-// typedef struct dealer_message {
-//   char message[32]; //message
-//   int player_state;  //state number 0 = init , 1=bet , 2 = playing, 3 = waiting
-// } dealer_message;
-
-// typedef struct result_message {
-//   char message[32]; //message
-//   int result;  //result number 0 = lose, 1 = win, 2 = draw
-// } result_message;
-
-// ready_message readyMessage;
-// bet_message betMessage;
-// hit_card_message hitMessage;
-// draw_card_message drawCard;
-// dealer_message stateChange;
-// result_message result;
-
-
 typedef struct game_state_message {
   int state;
   int id;
@@ -88,39 +47,6 @@ game_state_message gameStateMessage;
 dealer_message dealerMessage;
 
 esp_now_peer_info_t peerInfo;
-
-//Recieve cards data and display
-// void CardRecieve(const uint8_t * mac, const uint8_t *incomingData, int len){
-//   memcpy(&drawCard, incomingData, sizeof(drawCard));
-
-//   display.clearDisplay();
-
-//   display.setTextSize(1);
-//   display.setTextColor(WHITE);
-//   display.setCursor(0,0);
-//   display.println("Your cards: ");
-
-//   display.setTextSize(1);
-//   display.setTextColor(WHITE);
-//   display.setCursor(0,9);
-//   display.print(drawCard.card[0]);
-//   display.print(" ");
-//   display.print(drawCard.card[1]);
-//   for(int i=2;i<5;i++){
-//     if(drawCard.card[i]!=0){
-//       display.print(drawCard.card[i]);
-//       display.print(" ");
-//     }
-//   }
-//   display.println("");
-
-//   display.setTextSize(1);
-//   display.setTextColor(WHITE);
-//   display.setCursor(0,27);
-//   display.println("Hit or Stand");
-
-//   display.display();
-// }
 
 //Start display
 void InitDisplay(){
@@ -203,16 +129,6 @@ void OnStateRecieve(const uint8_t * mac, const uint8_t *incomingData, int len){
   currentState = dealerMessage.player_state;
 }
 
-//Result recieve from dealer
-// void ResultRecieve(const uint8_t * mac, const uint8_t *incomingData, int len){
-//   memcpy(&result, incomingData, sizeof(result));
-// }
-
-// void OnReady(const uint8_t *mac_addr, esp_now_send_status_t status) {
-//   Serial.print("\r\nSend Ready Status:\t");
-//   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
-// }
-
 void handlePlayerIdleState();
 void handlePlayerPlaceBetState();
 void handlePlayerPlayingState();
@@ -264,99 +180,6 @@ void setup() {
   //Register receive callback
   esp_now_register_recv_cb(OnStateRecieve);
 }
-
-// void loop (){
-
-//   while(state==0){
-//     button1.loop();
-//     button2.loop();  
-//     // Serial.println(state);
-//     if(button1.isPressed()){
-//       Serial.println("state 0 button pressed");
-//       readyMessage.id=id;
-//       readyMessage.is_ready=1;
-//       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &readyMessage, sizeof(readyMessage));
-//       delay(100);
-//       break;
-//     }
-//   }
-
-//   InitToBetDisplay();
-//   while(stateChange.player_state==0){
-//     delay(10);
-//   }
-//   state = stateChange.player_state;
-
-//   Serial.println("state 1 begin");
-//   BetToPlayingDisplay();
-//   while(state==1){
-//     button1.loop();
-//     button2.loop();  
-//     if(button1.isPressed()){
-//       betMessage.id=id;
-//       betMessage.amount=10;
-//       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &betMessage, sizeof(betMessage));
-//       delay(100);
-//       break;
-//     }
-//   }
-
-//   while(stateChange.player_state==2){
-//     delay(10);
-//   }
-//   state = stateChange.player_state;
-  
-
-//   Serial.println("state 2 begin");
-//   esp_now_register_recv_cb(CardRecieve);
-//   while(state==2){
-//     button1.loop();
-//     button2.loop();  
-//     if(button1.isPressed()){
-//       hitMessage.id=id;
-//       hitMessage.hit=1;
-//       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &hitMessage, sizeof(hitMessage));
-//     }
-//     else if(button2.isPressed()){
-//       hitMessage.id=id;
-//       hitMessage.hit=0;
-//       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &hitMessage, sizeof(hitMessage));
-//       delay(100);
-//       break;
-//     }
-//   }
-
-//   while(stateChange.player_state==3){
-//   }
-//   state = stateChange.player_state;
-//   Serial.println("state 3 begin");
-
-//   while(state==3){
-//     button1.loop();
-//     button2.loop();  
-//     esp_now_register_recv_cb(ResultRecieve);
-//     display.clearDisplay();
-//     display.setTextSize(1);
-//     display.setTextColor(WHITE);
-//     display.setCursor(0,0);
-//     if (result.result == 0){
-//       display.println("You Lose");
-//     }
-//     else if (result.result == 1){
-//       display.println("You win");
-//     }
-//     else if (result.result == 2){
-//       display.println("Draw");
-//     }
-//     display.display();
-//     delay(10000);
-//     break;
-//   }
-
-//   // if(stateChange.player_state!=state){
-//   //     state = stateChange.player_state;
-//   // }
-// }
 
 void loop () {
   button1.loop();
